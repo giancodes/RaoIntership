4ee3788378745d85206c08d7316706433c5dd209
= <=>
2023-02-03 16:39:23 +0530

    My new Webapp Like mobile Calling pad that can appennd What we are click in the pad

---
+++ css/index.js
@@ -0,0 +1,7 @@
+let isClick = () => document.addEventListener('click' , function() {
+    let innerText = document.getElementsByClassName('flex-items');
+    // document.getElementById('txt').innerHTML = innerText;
+    documnet.write('hello');
+});
+isClick();
+x = 12;
